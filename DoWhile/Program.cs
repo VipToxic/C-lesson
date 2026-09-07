0e098dc36744641bbee03ca8367e097a520ab508
viptoxic <akimbekovbekzat6@gmail.com>
2026-09-07 23:15:00 +0500

    Начал изучать другие циклы например DoWhile

---
+++ DoWhile/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace DoWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            foreach(char c in "Tom")
+            {
+                Console.WriteLine(c);
+            }
+        }
+    }
+}
