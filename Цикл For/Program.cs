c07f3973262ab3ac0572b1e3917b38fc4d37644f
viptoxic <akimbekovbekzat6@gmail.com>
2026-09-09 22:43:17 +0500

    Сегодня мало практики немного изучу C#

--- Цикл For/Program.cs
+++ Цикл For/Program.cs
@@ -4,12 +4,22 @@
     {
         static void Main(string[] args)
         {
+            /*
             int lim = int.Parse(Console.ReadLine());
 
             for (int i = 0; i <= lim; i++)
             {
                 Console.WriteLine(i);
             }
+            */
+
+            for (;;)
+            {
+                Console.WriteLine("Вот так вот");
+                System.Threading.Thread.Sleep(300);
+            }
+
+            Console.ReadLine();
         }
     }
 }
