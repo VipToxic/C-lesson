160353233ca578f976172cd5a6573d4e33a132b8
viptoxic <akimbekovbekzat6@gmail.com>
2026-09-13 23:29:57 +0500

    Еще одна практика наэтот раз узнал про Console.SetCursorPosition

--- Вложенные циклы/Program.cs
+++ Вложенные циклы/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            /*
+            
             for (int i = 1; i <= 3; i++)
             {
                 for (int j = 1; j <= 2; j++)
@@ -45,7 +45,7 @@
                 }
                 Console.WriteLine();
             }
-            */
+             
             for (int i = 1; i <= 5; i++)
             {
                 for (int j = 1; j <= i; j++)
---
+++ Курсор триугольник/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Курсор_триугольник
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("------ПЕРВЫЙ МЕТОД------");
+            Console.Write("Введите высоту: ");
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= height; i++)
+            {
+                for (int j = 1; j <= height - i; j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            
+            Console.WriteLine("------ВТОРОЙ МЕТОД------");
+            Console.Write("Введите высоту: ");
+            int height_1 = int.Parse(Console.ReadLine());
+            
+            for (int i = 0; i <= height_1; i++)
+            {
+                Console.SetCursorPosition(height_1 - i, i + 1);
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+            
+
+
+
+        }
+    }
+}
