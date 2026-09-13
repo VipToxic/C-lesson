14fa4ebdd6ff78ed9f2c2ed01d8a6481639101ac
viptoxic <akimbekovbekzat6@gmail.com>
2026-09-13 12:11:33 +0500

    Начел изучение вложенных циклов

---
+++ Вложенные циклы/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Вложенные_циклы
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
