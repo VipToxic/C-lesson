7b274480253527be10f5f592d37ec3471ad4f6ca
viptoxic <akimbekovbekzat6@gmail.com>
2026-09-13 22:59:55 +0500

    Последнее практика по for

--- Вложенные циклы/Program.cs
+++ Вложенные циклы/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            /*
             for (int i = 1; i <= 3; i++)
             {
                 for (int j = 1; j <= 2; j++)
@@ -44,6 +45,17 @@
                 }
                 Console.WriteLine();
             }
+            */
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+
+                Console.WriteLine();
+            }
+
         }
     }
 }
