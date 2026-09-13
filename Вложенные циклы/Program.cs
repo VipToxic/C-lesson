8a3ebacd14a2d7140f17af7e57103e358861e49c
viptoxic <akimbekovbekzat6@gmail.com>
2026-09-13 13:14:11 +0500

    Небольшая практика по вложенным циклам

--- Вложенные циклы/Program.cs
+++ Вложенные циклы/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            for (int i = 1; i <= 3; i++)
+            {
+                for (int j = 1; j <= 2; j++)
+                {
+                    Console.WriteLine($"i = {i}, j = {j}");
+                }
+            }
+
+            Console.WriteLine("--------------------");
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("--------------------");
+
+            for (int i = 1; i <= 2; i++)
+            {
+                for (int j = 1; j <=5; j++)
+                {
+                    Console.WriteLine($"{i} * {j} = {i * j}");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("--------------------");
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.Write("# ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
